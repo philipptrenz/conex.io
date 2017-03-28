2a886ac7f087f16bbd7f7d3291e6a842b2c033df
Timo <timoschwan@gmx.de>
2017-03-28 21:21:45 +0200

    Ergänzung DevicesApiController mit Filter-Eigenschaft - ohne Test

--- src/main/java/io/swagger/api/DevicesApiController.java
+++ src/main/java/io/swagger/api/DevicesApiController.java
@@ -1,31 +1,27 @@
 package io.swagger.api;
 
+import io.swagger.model.Device;
 import io.swagger.model.Devices;
-import io.swagger.model.Error;
 import io.swagger.model.Filter;
+import io.swagger.model.Function;
 import io.swagger.model.Patcher;
 
 import io.swagger.annotations.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
-import javax.validation.constraints.*;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-03-22T14:12:09.728Z")
 
 @Controller
 public class DevicesApiController implements DevicesApi {
-
-
+	private Filter filter;
+	private List<Device> geraete;
 
     public ResponseEntity<Void> devicesPatch(@ApiParam(value = "Filter object with function values" ,required=true ) @RequestBody Patcher patcher) {
         // do some magic!
@@ -33,8 +29,124 @@ public class DevicesApiController implements DevicesApi {
     }
 
     public ResponseEntity<Devices> devicesPost(@ApiParam(value = "The user specified filter" ,required=true ) @RequestBody Filter filter) {
-        // do some magic!
+        this.filter = filter;
+        this.geraete = mapping.Main.getDevices();
+        List<Device> ergebnis = geraete;
+        
+        if(!filter.getDeviceIds().isEmpty()) {
+        	ergebnis = calcDevices(geraete);
+        }
+        else if (!filter.getFunctionIds().isEmpty()) {
+        	ergebnis = calcFunction(geraete);
+        }
+        else if (!filter.getGroupIds().isEmpty()) {
+        	ergebnis = calcGroup(geraete);
+        }
+        else if (!filter.getRoomIds().isEmpty()){
+        	ergebnis = calcRoom(geraete);
+        }
+        else {
+        	//return geraete;
+        }
+        //return ergebnis
         return new ResponseEntity<Devices>(HttpStatus.OK);
+    } 
+    private List<Device> calcDevices(List<Device> liste) {
+    	while(liste.iterator().hasNext()) {
+        	Device d = liste.iterator().next();
+			boolean abfrage = true;
+    		while(filter.getDeviceIds().iterator().hasNext()) {
+    			String deviceId = filter.getDeviceIds().iterator().next();
+    			if(d.getDeviceId().contains(deviceId)) {
+    				abfrage = false;
+    			}
+    		}
+    		if(abfrage) {
+    			liste.remove(d);
+    		}
+    		//Lösche Device aus Liste
+    	}
+    	if(liste.isEmpty()) {
+    		return liste;
+    	}
+    	if(!filter.getFunctionIds().isEmpty()) {
+    		liste = calcFunction(liste);
+    	}
+    	else if(!filter.getGroupIds().isEmpty()) {
+    		liste = calcGroup(liste);
+    	}
+    	else if(!filter.getRoomIds().isEmpty()) {
+    		liste = calcRoom(liste);
+    	}
+    	
+    	return liste;
     }
-
+    private List<Device> calcFunction(List<Device> liste) {
+    	while(liste.iterator().hasNext()) {
+        	Device d = liste.iterator().next();
+			boolean abfrage = true;
+    		while(filter.getFunctionIds().iterator().hasNext()) {
+    			String functionId = filter.getFunctionIds().iterator().next();
+    			if(d.getFunctions().contains(functionId)) {
+    				abfrage = false;
+    			
+    			}
+    		}
+    		if(abfrage) {
+    			liste.remove(d);
+    		}
+    		//Lösche Device aus Liste
+    	}
+    	if(liste.isEmpty()) {
+    		return liste;
+    	}
+    	if(!filter.getGroupIds().isEmpty()) {
+    		liste = calcGroup(liste);
+    	}
+    	else if(!filter.getRoomIds().isEmpty()) {
+    		liste = calcRoom(liste);
+    	}
+    	return liste;
+    }
+    private List<Device> calcGroup(List<Device> liste) {
+    	while(liste.iterator().hasNext()) {
+        	Device d = liste.iterator().next();
+			boolean abfrage = true;
+    		while(filter.getGroupIds().iterator().hasNext()) {
+    			String groupId = filter.getGroupIds().iterator().next();
+    			if(d.getGroupIds().contains(groupId)) {
+    				abfrage = false;
+    			
+    			}
+    		}
+    		if(abfrage) {
+    			liste.remove(d);
+    		}
+    		//Lösche Device aus Liste
+    	}
+    	if(liste.isEmpty()) {
+    		return liste;
+    	}
+    	if(!filter.getRoomIds().isEmpty()) {
+    		liste = calcRoom(liste);
+    	}
+    	return liste;
+    }
+    private List<Device> calcRoom(List<Device> liste) {
+    	while(liste.iterator().hasNext()) {
+        	Device d = liste.iterator().next();
+			boolean abfrage = true;
+    		while(filter.getRoomIds().iterator().hasNext()) {
+    			String roomId = filter.getRoomIds().iterator().next();
+    			if(d.getRoomIds().contains(roomId)) {
+    				abfrage = false;
+    			
+    			}
+    		}
+    		if(abfrage) {
+    			liste.remove(d);
+    		}
+    	}
+    	return liste;
+    }
 }
--- src/main/java/io/swagger/model/Filter.java
+++ src/main/java/io/swagger/model/Filter.java
@@ -2,12 +2,9 @@ package io.swagger.model;
 
 import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.util.ArrayList;
 import java.util.List;
-import javax.validation.constraints.*;
 /**
  * Filter
  */
