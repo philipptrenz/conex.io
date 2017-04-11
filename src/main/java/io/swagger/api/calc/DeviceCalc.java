d3b27d49e6684e44c69aee54fd1857f59f82f045
Timo <timoschwan@gmx.de>
2017-04-11 17:18:06 +0200

    - Fixed pom.xml to match spring codegen requirements - Fixed Filtering-System to faster methods - Fixed .swagger-codegen-ignore to comment-only based file (it's no longer needed right now, but remain)

--- src/main/java/io/swagger/api/calc/DeviceCalc.java
+++ src/main/java/io/swagger/api/calc/DeviceCalc.java
@@ -14,29 +14,85 @@ public class DeviceCalc {
 	private List<Device> ausgabe;
 	
 
+	/*
+	 * als HashMap ? Array in HashMap // oder Liste !
+	 * pro Device -> eine Function 
+	 */
 	public DeviceCalc(Filter filter) {
         this.filter = filter;
         this.geraete = mapping.Main.getDevices();
-        ausgabe = geraete;
-        if(!filter.getDeviceIds().isEmpty()) {
-        	ausgabe = calcDevices(geraete);
-        }
-        else if (!filter.getFunctionIds().isEmpty()) {
-        	ausgabe = calcFunction(geraete);
-        }
-        else if (!filter.getGroupIds().isEmpty()) {
-        	ausgabe = calcGroup(geraete);
-        }
-        else if (!filter.getRoomIds().isEmpty()){
-        	ausgabe = calcRoom(geraete);
-        }
-        else {}
 	}
 	public List<Device> getListFiltering() {
+        if(!filter.getDeviceIds().isEmpty() && !geraete.isEmpty()) {
+        	while(geraete.iterator().hasNext()) {
+            	Device d = geraete.iterator().next();
+    			boolean abfrage = true;
+        		while(filter.getDeviceIds().iterator().hasNext()) {
+        			String deviceId = filter.getDeviceIds().iterator().next();
+        			if(d.getDeviceId().contains(deviceId)) {
+        				abfrage = false;
+        			}
+        		}
+        		if(abfrage) {
+        			geraete.remove(d);
+        		}
+        	}
+        }
+        else if (!filter.getFunctionIds().isEmpty() && !geraete.isEmpty()) {
+        	while(geraete.iterator().hasNext()) {
+            	Device d = geraete.iterator().next();
+    			boolean abfrage = true;
+        		while(filter.getFunctionIds().iterator().hasNext()) {
+        			String functionId = filter.getFunctionIds().iterator().next();
+        			if(d.getFunctions().contains(functionId)) {
+        				abfrage = false;
+        			
+        			}
+        		}
+        		if(abfrage) {
+        			geraete.remove(d);
+        		}
+        	}
+        }
+        else if (!filter.getGroupIds().isEmpty() && !geraete.isEmpty()) {
+        	while(geraete.iterator().hasNext()) {
+            	Device d = geraete.iterator().next();
+    			boolean abfrage = true;
+        		while(filter.getGroupIds().iterator().hasNext()) {
+        			String groupId = filter.getGroupIds().iterator().next();
+        			if(d.getGroupIds().contains(groupId)) {
+        				abfrage = false;
+        			
+        			}
+        		}
+        		if(abfrage) {
+        			geraete.remove(d);
+        		}
+        	}
+        }
+        else if (!filter.getRoomIds().isEmpty() && !geraete.isEmpty()){
+        	while(geraete.iterator().hasNext()) {
+            	Device d = geraete.iterator().next();
+    			boolean abfrage = true;
+        		while(filter.getRoomIds().iterator().hasNext()) {
+        			String roomId = filter.getRoomIds().iterator().next();
+        			if(d.getRoomIds().contains(roomId)) {
+        				abfrage = false;
+        			
+        			}
+        		}
+        		if(abfrage) {
+        			geraete.remove(d);
+        		}
+        	}
+        }
+        else {}
+        ausgabe = geraete;
 		return ausgabe;
 	}
 	public List<String> getFuntionFiltering() {
 		List<String> functions = new ArrayList<String>();
+		getListFiltering();
 		while(ausgabe.iterator().hasNext()) {
 			Device d = ausgabe.iterator().next();
 			while(d.getFunctions().iterator().hasNext()) {
@@ -51,6 +107,7 @@ public class DeviceCalc {
 	
 	public List<String> getGroupFiltering() {
 		List<String> gruppen = new ArrayList<String>();
+		getListFiltering();
 		while(ausgabe.iterator().hasNext()) {
 			Device d = ausgabe.iterator().next();
 			while(d.getGroupIds().iterator().hasNext()) {
@@ -65,6 +122,7 @@ public class DeviceCalc {
 	
 	public List<String> getRoomFiltering() {
 		List<String> rooms = new ArrayList<String>();
+		getListFiltering();
 		while(ausgabe.iterator().hasNext()) {
 			Device d = ausgabe.iterator().next();
 			while(d.getRoomIds().iterator().hasNext()) {
@@ -76,104 +134,4 @@ public class DeviceCalc {
 		}
 		return rooms;
 	}
-	
-	
-    private List<Device> calcDevices(List<Device> liste) {
-    	while(liste.iterator().hasNext()) {
-        	Device d = liste.iterator().next();
-			boolean abfrage = true;
-    		while(filter.getDeviceIds().iterator().hasNext()) {
-    			String deviceId = filter.getDeviceIds().iterator().next();
-    			if(d.getDeviceId().contains(deviceId)) {
-    				abfrage = false;
-    			}
-    		}
-    		if(abfrage) {
-    			liste.remove(d);
-    		}
-    		//Lösche Device aus Liste
-    	}
-    	if(liste.isEmpty()) {
-    		return liste;
-    	}
-    	if(!filter.getFunctionIds().isEmpty()) {
-    		liste = calcFunction(liste);
-    	}
-    	else if(!filter.getGroupIds().isEmpty()) {
-    		liste = calcGroup(liste);
-    	}
-    	else if(!filter.getRoomIds().isEmpty()) {
-    		liste = calcRoom(liste);
-    	}
-    	
-    	return liste;
-    }
-    private List<Device> calcFunction(List<Device> liste) {
-    	while(liste.iterator().hasNext()) {
-        	Device d = liste.iterator().next();
-			boolean abfrage = true;
-    		while(filter.getFunctionIds().iterator().hasNext()) {
-    			String functionId = filter.getFunctionIds().iterator().next();
-    			if(d.getFunctions().contains(functionId)) {
-    				abfrage = false;
-    			
-    			}
-    		}
-    		if(abfrage) {
-    			liste.remove(d);
-    		}
-    		//Lösche Device aus Liste
-    	}
-    	if(liste.isEmpty()) {
-    		return liste;
-    	}
-    	if(!filter.getGroupIds().isEmpty()) {
-    		liste = calcGroup(liste);
-    	}
-    	else if(!filter.getRoomIds().isEmpty()) {
-    		liste = calcRoom(liste);
-    	}
-    	return liste;
-    }
-    private List<Device> calcGroup(List<Device> liste) {
-    	while(liste.iterator().hasNext()) {
-        	Device d = liste.iterator().next();
-			boolean abfrage = true;
-    		while(filter.getGroupIds().iterator().hasNext()) {
-    			String groupId = filter.getGroupIds().iterator().next();
-    			if(d.getGroupIds().contains(groupId)) {
-    				abfrage = false;
-    			
-    			}
-    		}
-    		if(abfrage) {
-    			liste.remove(d);
-    		}
-    		//Lösche Device aus Liste
-    	}
-    	if(liste.isEmpty()) {
-    		return liste;
-    	}
-    	if(!filter.getRoomIds().isEmpty()) {
-    		liste = calcRoom(liste);
-    	}
-    	return liste;
-    }
-    private List<Device> calcRoom(List<Device> liste) {
-    	while(liste.iterator().hasNext()) {
-        	Device d = liste.iterator().next();
-			boolean abfrage = true;
-    		while(filter.getRoomIds().iterator().hasNext()) {
-    			String roomId = filter.getRoomIds().iterator().next();
-    			if(d.getRoomIds().contains(roomId)) {
-    				abfrage = false;
-    			
-    			}
-    		}
-    		if(abfrage) {
-    			liste.remove(d);
-    		}
-    	}
-    	return liste;
-    }
 }
