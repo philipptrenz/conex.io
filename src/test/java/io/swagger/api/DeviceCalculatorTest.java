865a377d9f9a7e7bd79c68bf257e6470f721de8c
Timo <timoschwan@gmx.de>
2017-04-24 12:23:03 +0200

    Added base model for DeviceCalc Testing (Unit-Test)

---
+++ src/test/java/io/swagger/api/DeviceCalculatorTest.java
@@ -0,0 +1,61 @@
+package io.swagger.api;
+
+import org.junit.*;
+
+public class DeviceCalculatorTest {
+	
+	/**
+	 * Devices
+	 */
+	
+	@Test
+	public void getDevicesByAllFilters() {
+		//TODO add method logic
+	}
+	@Test
+	public void getDevicesByDeviceOnly() {
+		//TODO add method logic
+	}
+	@Test
+	public void getDevicesByFunctionOnly() {
+		//TODO add method logic
+	}
+	@Test
+	public void getDevicesByGroupOnly() {
+		//TODO add method logic
+	}
+	@Test
+	public void getDevicesByRoomOnly() {
+		//TODO add method logic
+	}
+	@Test
+	public void getDevicesByMultipleDifferentFilters() {
+		//TODO add method logic
+	}
+	@Test
+	public void getAllDevicesByNoneFiltering() {
+		//TODO add method logic
+	}
+	@Test
+	public void getNoDevices() {
+		//TODO add method logic
+	}
+
+	
+	/**
+	 * Functions
+	 */
+	//TODO copy devices -> functions
+	
+	
+	/**
+	 * Groups
+	 */
+	//TODO copy devices -> groups
+	
+	
+	/**
+	 * Rooms
+	 */
+	//TODO copy devices -> rooms
+}
