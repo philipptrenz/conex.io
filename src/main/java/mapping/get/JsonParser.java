dbc7a756baa31e35a5753a72c1d6bc76dad8c5a5
philipptrenz <mail@philipptrenz.de>
2017-05-07 20:46:25 +0200

    Small bug fix

--- src/main/java/mapping/get/JsonParser.java
+++ src/main/java/mapping/get/JsonParser.java
@@ -1,7 +1,5 @@
 package mapping.get;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
