651f1026a35b4ad2fbd9bf9a1e7465090b9c7d0f
Timo <u30357@hs-harz.de>
2017-07-10 14:48:42 +0200

    Fixed error for function == null check

--- src/main/java/io/swagger/api/calc/DeviceCalc.java
+++ src/main/java/io/swagger/api/calc/DeviceCalc.java
@@ -70,7 +70,7 @@ public class DeviceCalc {
 	 * @return the device list filtering with patcher function
 	 */
 	public List<Device> getDeviceListFilteringWithPatcherFunction(Function function) {
-		if (function == null || function.getFunctionId() == null) {
+		if (function == null || function != null || function.getFunctionId() == null) {
 			function = new Function();
 			function.setFunctionId("null");
 		}
