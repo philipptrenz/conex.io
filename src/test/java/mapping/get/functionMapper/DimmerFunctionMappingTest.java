73db936b0b415953d743360614d29b02c005daa0
Timo <u30357@hs-harz.de>
2017-07-19 17:16:06 +0200

    Added / fixed annotations to match spring testing requirements

--- src/test/java/mapping/get/functionMapper/DimmerFunctionMappingTest.java
+++ src/test/java/mapping/get/functionMapper/DimmerFunctionMappingTest.java
@@ -3,13 +3,34 @@ package mapping.get.functionMapper;
 import java.io.IOException;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 
+import io.swagger.Swagger2SpringBoot;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@IntegrationTest
+@WebAppConfiguration
+@SpringApplicationConfiguration(classes = Swagger2SpringBoot.class)
 public class DimmerFunctionMappingTest {
+	
+	@Autowired
+	ApplicationContext context;
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	private String json = "{\"function_id\":\"dimmer\",\"value\":\"127\",\"timestamp\":\"2016-04-24T16:02:18.000Z\"}";
--- src/test/java/mapping/get/functionMapper/JacksonTimestampMappingTest.java
+++ src/test/java/mapping/get/functionMapper/JacksonTimestampMappingTest.java
@@ -4,6 +4,18 @@ import java.io.IOException;
 
 import org.joda.time.DateTime;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -12,7 +24,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 
+import io.swagger.Swagger2SpringBoot;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@IntegrationTest
+@WebAppConfiguration
+@SpringApplicationConfiguration(classes = Swagger2SpringBoot.class)
 public class JacksonTimestampMappingTest {
+	
+	@Autowired
+	ApplicationContext context;
 
     private static final String json = "{ \n" +
             "  \"timestamp\" : \"2014-08-20T11:51:31.233Z\" \n" +
